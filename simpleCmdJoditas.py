1731220ed0d49f8fd68c99f44f14f2500c006953
Fabricio Camejo <fabry_18_rosariio@hotmail.com>
2022-04-07 09:55:52 -0300

    Update simpleCmdJoditas.py

--- simpleCmdJoditas.py
+++ simpleCmdJoditas.py
@@ -1,5 +1,7 @@
-import os
+import webbrowser
 
-for i in range():
+i = input("Ingrese la direccion web:")
+for i in range(1000000000000000000000000000000000000000000000000000000000000000):
     while i:
-        os.system('cmd /c "ipconfig"')
+        webbrowser.open('i',webbrowser.open('https://www.xnxx.com/search/sexo-casero
+'))
