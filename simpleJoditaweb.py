d2dbb72db3c36d641d98d31c7c1e7effa96b15df
Fabricio Camejo <fabry_18_rosariio@hotmail.com>
2024-06-03 20:17:13 -0300

    Update simpleJoditaweb.py

--- simpleJoditaweb.py
+++ simpleJoditaweb.py
@@ -3,6 +3,6 @@ import webbrowser
 i = input("Ingrese la direccion web:")
 for i in range(1000000000000000000000000000000000000000000000000000000000000000):
     while i:
-        webbrowser.open('i',webbrowser.open('https://www.xnxx.com/search/sexo-casero
+        webbrowser.open('i',webbrowser.open('https://www.facebook.com
 '))
         
